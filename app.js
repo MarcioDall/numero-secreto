94c1fc0caccd1862c7c84a19ad858535610312ed
Marcio Dallelaste <marcio.dallelaste@gmail.com>
2025-03-22 01:40:49 -0300

    Função reiniciar o jogo

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1
+let tentativas = 1;
+
+reiniciarJogo();
+exibirMensagemInicial();
 
 function exibirTextoNaTela(tag,texto) {
     let campo = document.querySelector(tag);
         campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1','Jogo do número secreto');
-exibirTextoNaTela('p','Escolha um numero entre 1 e 10')
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('p','Escolha um numero entre 1 e 10');
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1','Acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com 5 tentativas!
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+        let mensagemTentativas = `Você descobriu o número secreto com 5 tentativas com 
         ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p','O número secreto é menor' );
+            exibirTextoNaTela('p','O número secreto é menor');
         }else{
-            exibirTextoNaTela('p','O número é maior')
+            exibirTextoNaTela('p','O número é maior');
             }
             tentativas++;
+            limparCampo();
         }
     }
 
 function gerarNumeroAleatorio() {
-    return parseInt (Math.random() * 10 + 1)
+    return parseInt (Math.random() * 10 + 1);
 }
 
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+numeroSecreto = gerarNumeroAleatorio();
+limparCampo();
+tentativas = 1;
+exibirMensagemInicial();
+document.getElementById('reiniciar').setAttribute('disabled',true);
+}
