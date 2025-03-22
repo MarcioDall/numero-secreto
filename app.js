a6649a114467b08ac4feeace8454fae7fa2e0d31
Marcio Dallelaste <marcio.dallelaste@gmail.com>
2025-03-22 00:54:43 -0300

    Refinamento das clades de comparação

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ exibirTextoNaTela('h1','Jogo do número secreto');
 exibirTextoNaTela('p','Escolha um numero entre 1 e 10')
 
 function verificarChute() {
+
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-}
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1','Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p','O número secreto é menor' );
+        }else{
+            exibirTextoNaTela('p','O número é maior')
+            }
+        }
+    }
 
 function gerarNumeroAleatorio() {
     return parseInt (Math.random() * 10 + 1)
